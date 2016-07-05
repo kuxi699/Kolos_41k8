3d2b0ea3556115bb00d7a648b95ad7ae497c915b
Lukasz <kuxi69@gmail.com>
2016-07-04 22:30:26 -0800

    Dodanie funkcji obliczajacej maxymalny produkt 3 elementow

--- Kolos_41K8/Program.cs
+++ Kolos_41K8/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Kolos_41K8
 {
-    class Program
+     public class Program
     {
+
+        public static void Rozwiazanie(int P,int Q,int R,int []A)
+        {
+           
+            int Roz;
+            Roz = A[P]*A[Q]*A[R];
+           
+            Console.Write(Roz);
+           
+
+        }
+
+
         static void Main(string[] args)
         {
+
+            int[] A = { -3, 1, 2, -2, 5, 6 };
+            Console.WriteLine(string.Join(",", A));
+            Rozwiazanie(2, 4, 5,A);
+            Console.ReadLine();
+
         }
     }
 }
